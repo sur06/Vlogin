b562a83ab4885c4f571d248fed102fd56896f60a
sur06 <surabhi.suresh65@gmail.com>
2017-03-20 18:50:05 +0530

    Update Appconfig.java

--- app/src/main/java/com/example/girivi/vlogin/Appconfig.java
+++ app/src/main/java/com/example/girivi/vlogin/Appconfig.java
@@ -5,6 +5,6 @@ package com.example.girivi.vlogin;
 public class Appconfig {
 
     public static final String LOGIN_URL = "http://triphpe.16mb.com/nlog.php";
-    public static final String REGISTER_URL = "http://triphpe.16mb.com/vreg.php";
+    public static final String REGISTER_URL = "http://triphpe.16mb.com/vreg.php"; //sample
 
 }
